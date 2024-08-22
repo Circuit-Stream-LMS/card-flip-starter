69811d6330176c59a7eff83e2ec284a5a7dc29eb
Ben Organ <benorgan@gmail.com>
2024-08-21 18:29:19 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+(function () {
+    let clicks = 0
+    let found = 0
+
+    const clickCounter = document.getElementById('clicks')
+    const foundCounter = document.getElementById('found')
+
+    const shuffle = (array) => {
+
+    }
+
+    const reset = () => {
+
+    }
+
+    const clickHandler = (card) => {
+
+    }
+
+    // Reset the game at the start
+    reset()
+})()
